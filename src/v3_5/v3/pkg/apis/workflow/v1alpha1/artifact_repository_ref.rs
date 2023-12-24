a05a41decc2b9a286d6f46701f3686e8337a65c0
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2023-12-25 00:49:44 +0530

    add type - Artifact, and related
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ src/v3_5/v3/pkg/apis/workflow/v1alpha1/artifact_repository_ref.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ArtifactRepositoryRef {
+    /// The name of the config map. Defaults to \"artifact-repositories\".
+    #[serde(rename = "configMap", skip_serializing_if = "Option::is_none")]
+    pub config_map: Option<String>,
+
+    /// The config map key. Defaults to the value of the
+    /// \"workflows.argoproj.io/default-artifact-repository\" annotation.
+    #[serde(rename = "key", skip_serializing_if = "Option::is_none")]
+    pub key: Option<String>,
+}
+
+impl ArtifactRepositoryRef {
+    pub fn new() -> ArtifactRepositoryRef {
+        ArtifactRepositoryRef {
+            config_map: None,
+            key: None,
+        }
+    }
+}
