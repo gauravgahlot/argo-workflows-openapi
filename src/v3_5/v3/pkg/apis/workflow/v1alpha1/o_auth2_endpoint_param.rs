4efb159e5088b46f0923208a842d7544227e024c
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2023-12-25 00:11:41 +0530

    add type - OAuth2Auth, OAuth2EndpointParam
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ src/v3_5/v3/pkg/apis/workflow/v1alpha1/o_auth2_endpoint_param.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+/// EndpointParam is for requesting optional fields that should
+/// be sent in the oauth request.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct OAuth2EndpointParam {
+    /// Name is the header name
+    #[serde(rename = "key")]
+    pub key: String,
+    /// Value is the literal value to use for the header
+    #[serde(rename = "value", skip_serializing_if = "Option::is_none")]
+    pub value: Option<String>,
+}
+
+impl OAuth2EndpointParam {    
+    pub fn new(key: String) -> OAuth2EndpointParam {
+        OAuth2EndpointParam {
+            key,
+            value: None,
+        }
+    }
+}
