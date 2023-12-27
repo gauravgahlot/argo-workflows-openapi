efb9387a48bb413471ff7d84e943a9f9f7897c34
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2023-12-27 16:55:01 +0530

    add types for node status
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ src/v3_5/v3/pkg/apis/workflow/v1alpha1/condition.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Condition {
+    /// Message is the condition message.
+    #[serde(rename = "message", skip_serializing_if = "Option::is_none")]
+    pub message: Option<String>,
+
+    /// Status is the status of the condition.
+    #[serde(rename = "status", skip_serializing_if = "Option::is_none")]
+    pub status: Option<String>,
+
+    /// Type is the type of condition.
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    pub r#type: Option<String>,
+}
+
+impl Condition {
+    pub fn new() -> Condition {
+        Condition {
+            message: None,
+            status: None,
+            r#type: None,
+        }
+    }
+}
