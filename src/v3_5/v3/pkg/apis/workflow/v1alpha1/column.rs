9c2006506c2e1675f24b92a64e1f87f540097727
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2024-01-26 16:33:13 +0530

    marks
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ src/v3_5/v3/pkg/apis/workflow/v1alpha1/column.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+// Column is a custom column that will be exposed in the Workflow List View.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Column {
+    /// The key of the label or annotation, e.g., \"workflows.argoproj.io/completed\".
+    #[serde(rename = "key")]
+    pub key: String,
+
+    /// The name of this column, e.g., \"Workflow Completed\".
+    #[serde(rename = "name")]
+    pub name: String,
+
+    /// The type of this column, \"label\" or \"annotation\".
+    #[serde(rename = "type")]
+    pub r#type: String,
+}
+
+impl Column {
+    pub fn new(key: String, name: String, r#type: String) -> Self {
+        Column {
+            key,
+            name,
+            r#type,
+        }
+    }
+}
+
